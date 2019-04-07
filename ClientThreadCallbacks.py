40609849d6db55c4f2be8e3bcb9854bac5c32435
Muroming <ulamog345@gmail.com>
2019-04-07 13:26:10 +0300

    Saving user

--- ClientThreadCallbacks.py
+++ ClientThreadCallbacks.py
@@ -4,9 +4,10 @@ from enum import Enum
 
 import numpy as np
 
-from NeuralNets.FaceRecognition.Recognition import extract_face_from_image
+from NeuralNets.FaceRecognition.Recognition import *
 
 cache_folder = "cache"
+dataset_folder = "./NeuralNets/FaceRecognition/encodings"
 
 
 class ClientThreadResponse(Enum):
@@ -30,8 +31,18 @@ def add_user_callback(image, username):
     filename = "%s_%s.npy" % (username, str(uuid.uuid4()))
     np.save(os.path.join(cache_folder, filename), encoding)
 
-    # TODO: save embeddings if gathered enough faces
-    if len(list(filter(lambda x: username in x, os.listdir(cache_folder)))) == 0:
+    user_faces = list(filter(lambda x: username in x, os.listdir(cache_folder)))
+    if len(user_faces) == person_faces_amount:
+        user_dataset = []
+        for user_face in user_faces:
+            user_dataset.append(np.load(os.path.join(cache_folder, user_face)))
+
+        user_dataset_np = np.vstack(user_dataset)
+        np.save(os.path.join(dataset_folder, username), user_dataset_np)
+
+        for user_face in user_faces:
+            os.remove(os.path.join(cache_folder, user_face))
+
         print("Person saved!")
         return ClientThreadResponse.CLOSE_SOCKET
 
